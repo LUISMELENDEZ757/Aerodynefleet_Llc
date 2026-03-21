1bb7876b1beae161c5dc7060ec6f68980cc982db
Luis Melendez <elzorro4u@gmail.com>
2026-03-21 01:52:09 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import LeftRail from './LeftRail';
 import BottomTabBar from './BottomTabBar';
 import { RailProvider, useRail } from '@/lib/RailContext';
+import { TabHistoryProvider, useTabHistory } from '@/lib/TabHistoryContext';
 import PageTransition from '@/components/ui/PageTransition';
+
+// Syncs location changes into TabHistoryContext so lastPaths stays accurate
+function LocationSync() {
+  const location = useLocation();
+  const { recordPath } = useTabHistory();
+  useEffect(() => {
+    recordPath(location.pathname);
+  }, [location.pathname]);
+  return null;
+}
 
 function AppContent() {
   const { expanded } = useRail();
   return (
     <div className="min-h-screen bg-background flex">
-      {/* Desktop side rail */}
+      <LocationSync />
       <LeftRail />
-
-      {/* Page content */}
       <div className={`flex-1 min-h-screen transition-all duration-300 ${expanded ? 'lg:ml-44' : 'lg:ml-16'}`}>
         <main className="pb-safe-bottom">
           <PageTransition>
@@ -20,8 +29,6 @@ function AppContent() {
           </PageTransition>
         </main>
       </div>
-
-      {/* Mobile bottom tab bar */}
       <BottomTabBar />
     </div>
   );
@@ -30,7 +37,9 @@ function AppContent() {
 export default function AppLayout() {
   return (
     <RailProvider>
-      <AppContent />
+      <TabHistoryProvider>
+        <AppContent />
+      </TabHistoryProvider>
     </RailProvider>
   );
 }
