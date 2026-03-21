7b5a3550ead37609c7ca37a63856150310af4c8c
Luis Melendez <elzorro4u@gmail.com>
2026-03-21 01:45:22 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import LeftRail from './LeftRail';
+import BottomTabBar from './BottomTabBar';
 import { RailProvider, useRail } from '@/lib/RailContext';
 import PageTransition from '@/components/ui/PageTransition';
 
@@ -8,7 +9,10 @@ function AppContent() {
   const { expanded } = useRail();
   return (
     <div className="min-h-screen bg-background flex">
+      {/* Desktop side rail */}
       <LeftRail />
+
+      {/* Page content */}
       <div className={`flex-1 min-h-screen transition-all duration-300 ${expanded ? 'lg:ml-44' : 'lg:ml-16'}`}>
         <main className="pb-safe-bottom">
           <PageTransition>
@@ -16,6 +20,9 @@ function AppContent() {
           </PageTransition>
         </main>
       </div>
+
+      {/* Mobile bottom tab bar */}
+      <BottomTabBar />
     </div>
   );
 }
